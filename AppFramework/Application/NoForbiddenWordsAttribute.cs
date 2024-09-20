ff27f965477b77b5cda46e290bf71d5ce72b9457
mardibak <mardibaki@gmail.com>
2024-09-20 16:38:46 +0330

    NoForbiddenWordsAttribute implemented

---
+++ AppFramework/Application/NoForbiddenWordsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace AppFramework.Application;
+
+public class NoForbiddenWordsAttribute : ValidationAttribute
+{
+    public string[] ForbiddenWords { get; set; }
+
+    public NoForbiddenWordsAttribute(params string[] forbiddenWords)
+    {
+        ForbiddenWords = forbiddenWords;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var stringValue = value as string;
+
+        if (!string.IsNullOrEmpty(stringValue))
+        {
+            foreach (var word in ForbiddenWords)
+            {
+                if (stringValue.Contains(word, StringComparison.OrdinalIgnoreCase))
+                {
+                    return new ValidationResult($"عبارت '{word}' مجاز نیست.");
+                }
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
--- ToDo.Application/DTOs/TaskItems/CreateTaskDto.cs
+++ ToDo.Application/DTOs/TaskItems/CreateTaskDto.cs
@@ -1,14 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AppFramework.Application;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDo.Application.DTOs.TaskItems;
 
-public class CreateTaskDto
+public class CreateTaskDto : IValidatableObject
 {
-    [Required(ErrorMessage = "عنوان را وارد کنید")]
-    [StringLength(100, MinimumLength = 3, ErrorMessage = "بین ۳ تا ۱۰۰ حرف")]
+    [Required(ErrorMessage = "Enter the title.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Between 3 and 100 characters")]
+    [NoForbiddenWords("fuck", "bitch", ErrorMessage = "The title contains illegal words.")]
     public string Title { get; set; }
     [StringLength(500)]
     public string Description { get; set; }
     public long TaskListId { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Title != null && Title.Contains("test"))
+        {
+            yield return new ValidationResult("The word 'test' is not allowed.", new[] { nameof(Title) });
+        }
+    }
 }
 
